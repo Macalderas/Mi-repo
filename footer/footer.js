18b698ee3522d7ad5e022e974a7dfff55c27ec66
-Mynor Calderas <macalderas@scl.edu.gt>
2024-05-13 22:54:09 -0600

    avanze

---
+++ footer/footer.js
@@ -0,0 +1,20 @@
+const cuadro = document.createElement('div');
+cuadro.classList.add('cuadro');
+
+const titulo = document.createElement('div');
+titulo.textContent = 'footer ';
+titulo.classList.add('titulo');
+
+const footer = document.createElement('footer');
+footer.classList.add('footer');
+
+const parrafo = document.createElement('p');
+parrafo.textContent = 'Encuéntranos en:';
+footer.appendChild(parrafo);
+
+const enlace = document.createElement('a');
+enlace.href = 'https://www.pc.com';
+enlace.textContent = 'www.pc.com';
+parrafo.appendChild(enlace);
+
+document.body.appendChild(footer);
--- header/header.js
+++ header/header.js
@@ -1,6 +1,44 @@
-let div_header = document.querySelector("header")
+const cuadro = document.createElement('div');
+cuadro.classList.add('cuadro');
 
-div_header.innerHTML = "header";
-<div>
-    <h2>las computadoras </h2>
-</div>
+const titulo = document.createElement('div');
+titulo.textContent = 'Componentes de la pc ';
+titulo.classList.add('titulo');
+
+const nav = document.createElement('nav');
+nav.classList.add('navegacion');
+
+const ul = document.createElement('ul');
+const items = ['Inicio', 'Acerca de', 'Cambio'];
+items.forEach(item => {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  a.href = '#';
+  a.textContent = item;
+  li.appendChild(a);
+  ul.appendChild(li);
+});
+nav.appendChild(ul);
+
+
+cuadro.appendChild(titulo);
+cuadro.appendChild(nav);
+
+
+document.body.appendChild(cuadro);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
