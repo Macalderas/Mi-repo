0264c05834e9cc5ffe02dcd513ddd5b2813792ef
-Mynor Calderas <macalderas@scl.edu.gt>
2024-05-16 10:24:39 -0600

    nuevas carpetas

--- footer/footer.js
+++ footer/footer.js
@@ -1,20 +1,2 @@
-const cuadro = document.createElement('div');
-cuadro.classList.add('cuadro_2');
-
-const titulo = document.createElement('div');
-titulo.textContent = 'footer ';
-titulo.classList.add('titulo');
-
 const footer = document.createElement('footer');
-footer.classList.add('footer');
-
-const parrafo = document.createElement('p');
-parrafo.textContent = 'Encuéntranos en:';
-footer.appendChild(parrafo);
-
-const enlace = document.createElement('a');
-enlace.href = 'https://www.pc.com';
-enlace.textContent = 'www.pc.com';
-parrafo.appendChild(enlace);
-
-document.body.appendChild(footer);
+footer.id = 'Footer';
