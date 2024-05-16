c31c078bbd0e8d089443cf4374432c382f23e6fa
miranda <mjjacobo@scl.edu.gt>
2024-05-16 11:36:16 -0600

    menu

--- menu_lateral/menu_lateral.js
+++ menu_lateral/menu_lateral.js
@@ -1,1 +1,16 @@
-.
+let contenido =document.querySelector(".menu-lateral");
+contenido.innerHTML =`
+<div class="contenido">
+<nav>
+  <ul class="menu">
+    <li><a href="index.html">Index</a></li>
+    <li><a href="cpu.html">CPU</a></li>
+    <li><a href="disco.html">Disco</a></li>
+    <li><a href="fuente.html">Fuente</a></li>
+    <li><a href="gpu.html">GPU</a></li>
+    <li><a href="ram.html">RAM</a></li>
+    <li><a href="tajeta.html">Tajeta</a></li>
+  </ul>
+</nav>
+</div>
+`;
