d4b924a52a5fe3e137aff0b0826d944a62992048
-Mynor Calderas <macalderas@scl.edu.gt>
2024-05-16 11:21:08 -0600

    mcalderas

---
+++ header/header.js
@@ -0,0 +1,31 @@
+let mcalderas=document.querySelector(".base");
+
+mcalderas.innerHTML=`
+<h1> mcalderas</h1>
+    <nav>
+        <ul>
+            <li><a href="#">acerca de nosotros</a></li>
+            <li><a href="#">relaciones laborales</a></li>
+            <li><a href="#">buen ambiente</a></li>
+            <li><a href="#">i love you</a></li>
+        </ul>
+    </nav>
+ 
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
