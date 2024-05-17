678477ec446db3c887e1bbb41734c5ab0065f1e2
-Mynor Calderas <macalderas@scl.edu.gt>
2024-05-16 21:19:26 -0600

    terminado

--- footer/footer.js
+++ footer/footer.js
@@ -4,16 +4,31 @@ footer.innerHTML=`
 <footer>
 <div class="abajo">
     <div class="abajo_logo">
-        <img src="scl.jpg" alt="Logo ">
+    <img src="scl.jpg" alt="Logo ">
     </div>
     <div class="abajo_redes-sociales">
     <a href="#" class="icono-red-social"><i class="fab fa-facebook-f"></i></a>
     <a href="#" class="icono-red-social"><i class="fab fa-twitter"></i></a>
     <a href="#" class="icono-red-social"><i class="fab fa-instagram"></i></a>
-</div>
-    <div class="abajo_stacks">
-        <p>Stacks utilizados: HTML, CSS, JavaScript</p>
     </div>
+    <div class="abajo_desarrollador">
+    (<span>Mac</span>)<br>
+    4to computacion<br>
+    a href="https://github.com/Macalderas">Perfil de GitHub</a>
+    </label>
+    </div>
+
+    <div class="abajo_desarrollador">
+    (<span>Miranda</span>)<br>
+    4to computacion<br>
+    a href="https://github.com/mirandajacobo">Perfil de GitHub</a>
+    </label>
+    </div>
+
+    <div class="abajo_stacks">
+        <p> HTML, CSS, JavaScript</p>
+    </div>
+   
 </div>
 </footer>
     `;
--- header/header.js
+++ header/header.js
@@ -1,13 +1,14 @@
 let mcalderas=document.querySelector(".base");
 
 mcalderas.innerHTML=`
-<h1> Componetes de computador </h1>
+<h1> Componetes de Computador </h1>
     <nav>
         <ul>
             <li><a href="#">acerca de nosotros</a></li>
             <li><a href="#">relaciones laborales</a></li>
             <li><a href="#">buen ambiente</a></li>
             <li><a href="#">inicio</a></li>
+            <a href="https://github.com/Macalderas/Mi-repo.git" class="boton-repositorio"> Repositorio</a> 
         </ul>
     </nav>
  
