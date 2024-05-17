6b0519604ad64e156c6608612c13a9ada757de4a
-Mynor Calderas <macalderas@scl.edu.gt>
2024-05-16 20:40:42 -0600

    Mac

--- footer/footer.js
+++ footer/footer.js
@@ -1,2 +1,19 @@
-const footer = document.createElement('footer');
-footer.id = 'Footer';
+let footer=document.querySelector(".abajo ");
+
+footer.innerHTML=`
+<footer>
+<div class="abajo">
+    <div class="abajo_logo">
+        <img src="scl.jpg" alt="Logo ">
+    </div>
+    <div class="abajo_redes-sociales">
+    <a href="#" class="icono-red-social"><i class="fab fa-facebook-f"></i></a>
+    <a href="#" class="icono-red-social"><i class="fab fa-twitter"></i></a>
+    <a href="#" class="icono-red-social"><i class="fab fa-instagram"></i></a>
+</div>
+    <div class="abajo_stacks">
+        <p>Stacks utilizados: HTML, CSS, JavaScript</p>
+    </div>
+</div>
+</footer>
+    `;
--- header/header.js
+++ header/header.js
@@ -1,13 +1,13 @@
 let mcalderas=document.querySelector(".base");
 
 mcalderas.innerHTML=`
-<h1> mcalderas</h1>
+<h1> Componetes de computador </h1>
     <nav>
         <ul>
             <li><a href="#">acerca de nosotros</a></li>
             <li><a href="#">relaciones laborales</a></li>
             <li><a href="#">buen ambiente</a></li>
-            <li><a href="#">i love you</a></li>
+            <li><a href="#">inicio</a></li>
         </ul>
     </nav>
  
